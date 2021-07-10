d0316547cbdfed0f0fa57a4e465265d47026d354
Filip Tijan <ftijan@gmail.com>
2021-07-10 15:55:15 +0100

    cleanup

--- src/Example.SignalR.Api/src/index.js
+++ src/Example.SignalR.Api/src/index.js
@@ -10,7 +10,6 @@ var connection = new signalR.HubConnectionBuilder()
     .withUrl("/hub")
     .build();
 connection.on("messageReceived", function (username, message) {
-    debugger;
     var m = document.createElement("div");
     m.innerHTML = "<div class=\"message-author\">" + username + "</div><div>" + message + "</div>";
     divMessages.appendChild(m);
@@ -19,13 +18,11 @@ connection.on("messageReceived", function (username, message) {
 connection.start().catch(function (err) { return document.write(err); });
 tbMessage.addEventListener("keyup", function (e) {
     if (e.key === "Enter") {
-        debugger;
         send();
     }
 });
 btnSend.addEventListener("click", send);
 function send() {
-    debugger;
     connection.send("newMessage", username, tbMessage.value)
         .then(function () { return tbMessage.value = ""; });
 }
